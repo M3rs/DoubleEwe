21753e7be8999f16c2ac4de2d47347f8b5280b27
Andy <morandyj@gmail.com>
2020-10-16 21:21:16 -0500

    initial commit

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            var speed = 1 * Time.deltaTime;
+            transform.position += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            var speed = -1 * Time.deltaTime;
+            transform.position += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            var speed = -1 * Time.deltaTime;
+            transform.position += new Vector3(0, speed, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            var speed = 1 * Time.deltaTime;
+            transform.position += new Vector3(0, speed, 0);
+        }
+
+        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Stop recording!");
+            var rec = GetComponent<RecordMovement>();
+            if (rec != null) {
+                rec.recording = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RecordMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordMovement : MonoBehaviour
+{
+    List<Vector3> positions;
+    int index;
+
+    public Transform player;
+    public Transform AI;
+    public bool recording;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = new List<Vector3>();
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (recording) {
+            //Debug.Log("Move?");
+            positions.Add(player.position);
+        } else {
+            if (index < positions.Count) {
+                AI.position = positions[index];
+                index++;
+            }
+        }
+    }
+}
